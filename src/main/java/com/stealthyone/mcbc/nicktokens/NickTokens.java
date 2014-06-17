9aee92e19b3f6a5dea47ba220b64a4efe93913b4
Stealth2800 <stealth2800@stealthyone.com>
2014-06-16 18:08:12 -0700

    Now ready to upload to BukkitDev. Cleanup, added update checker and metrics using mcstats.org.

--- src/main/java/com/stealthyone/mcbc/nicktokens/NickTokens.java
+++ src/main/java/com/stealthyone/mcbc/nicktokens/NickTokens.java
@@ -1,14 +1,35 @@
+/*
+ * NickTokens - Tokens for nickname changes using Essentials
+ * Copyright (C) 2014 Stealth2800 <stealth2800@stealthyone.com>
+ * Website: <http://stealthyone.com/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.stealthyone.mcbc.nicktokens;
 
 import com.stealthyone.mcb.stbukkitlib.messages.MessageManager;
 import com.stealthyone.mcb.stbukkitlib.players.PlayerUUIDTracker;
+import com.stealthyone.mcb.stbukkitlib.updates.UpdateChecker;
 import com.stealthyone.mcbc.nicktokens.backend.TokenManager;
 import com.stealthyone.mcbc.nicktokens.commands.CmdNickTokens;
 import com.stealthyone.mcbc.nicktokens.listeners.NicknameListener;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.mcstats.Metrics;
 
 import java.io.File;
+import java.io.IOException;
 
 public class NickTokens extends JavaPlugin {
 
@@ -17,6 +38,7 @@ public class NickTokens extends JavaPlugin {
     private MessageManager messageManager = new MessageManager(this);
     private PlayerUUIDTracker uuidTracker;
     private TokenManager tokenManager = new TokenManager(this);
+    private UpdateChecker updateChecker;
 
     private CmdNickTokens cmdNickTokens = new CmdNickTokens(this);
     private NicknameListener nicknameListener = new NicknameListener(this);
@@ -39,6 +61,14 @@ public class NickTokens extends JavaPlugin {
 
         getCommand("nicktokens").setExecutor(cmdNickTokens);
 
+        try {
+            Metrics metrics = new Metrics(this);
+            metrics.start();
+        } catch (IOException e) {
+            //Failed
+        }
+
+        updateChecker = new UpdateChecker(this, 81382);
         getLogger().info(String.format("%s v%s by Stealth2800 ENABLED.", getName(), getDescription().getVersion()));
     }
 
@@ -66,6 +96,10 @@ public class NickTokens extends JavaPlugin {
 
     public TokenManager getTokenManager() {
         return tokenManager;
+    }
+
+    public UpdateChecker getUpdateChecker() {
+        return updateChecker;
     }
 
 }
--- src/main/java/com/stealthyone/mcbc/nicktokens/commands/CmdNickTokens.java
+++ src/main/java/com/stealthyone/mcbc/nicktokens/commands/CmdNickTokens.java
@@ -1,9 +1,29 @@
+/*
+ * NickTokens - Tokens for nickname changes using Essentials
+ * Copyright (C) 2014 Stealth2800 <stealth2800@stealthyone.com>
+ * Website: <http://stealthyone.com/>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.stealthyone.mcbc.nicktokens.commands;
 
+import com.stealthyone.mcb.stbukkitlib.updates.UpdateChecker;
 import com.stealthyone.mcb.stbukkitlib.utils.QuickMap;
 import com.stealthyone.mcbc.nicktokens.NickTokens;
 import com.stealthyone.mcbc.nicktokens.messages.Messages;
 import com.stealthyone.mcbc.nicktokens.permissions.PermissionNode;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -24,11 +44,11 @@ public class CmdNickTokens implements CommandExecutor {
         if (args.length > 0) {
             switch (args[0].toLowerCase()) {
                 case "add":
-                    cmdAdd(sender, command, label, args);
+                    cmdAdd(sender, label, args);
                     return true;
 
                 case "check":
-                    cmdCheck(sender, command, label, args);
+                    cmdCheck(sender, label, args);
                     return true;
 
                 case "help":
@@ -36,19 +56,23 @@ public class CmdNickTokens implements CommandExecutor {
                     return true;
 
                 case "reload":
-                    cmdReload(sender, command, label, args);
+                    cmdReload(sender);
                     return true;
 
                 case "remove":
-                    cmdRemove(sender, command, label, args);
+                    cmdRemove(sender, label, args);
                     return true;
 
                 case "reset":
-                    cmdReset(sender, command, label, args);
+                    cmdReset(sender, label, args);
                     return true;
 
                 case "save":
-                    cmdSave(sender, command, label, args);
+                    cmdSave(sender);
+                    return true;
+
+                case "version":
+                    cmdVersion(sender);
                     return true;
 
                 default:
@@ -63,7 +87,7 @@ public class CmdNickTokens implements CommandExecutor {
     /*
      * Add tokens to a player's balance.
      */
-    private void cmdAdd(CommandSender sender, Command command, String label, String[] args) {
+    private void cmdAdd(CommandSender sender, String label, String[] args) {
         if (!PermissionNode.TOKENS_ADD.isAllowedAlert(sender)) return;
 
         if (args.length < 2) {
@@ -113,7 +137,7 @@ public class CmdNickTokens implements CommandExecutor {
     /*
      * Check a player's balance.
      */
-    private void cmdCheck(CommandSender sender, Command command, String label, String[] args) {
+    private void cmdCheck(CommandSender sender, String label, String[] args) {
         if (!PermissionNode.TOKENS_CHECK.isAllowedAlert(sender)) return;
 
         String targetName;
@@ -146,7 +170,7 @@ public class CmdNickTokens implements CommandExecutor {
     /*
      * Reload plugin configuration.
      */
-    private void cmdReload(CommandSender sender, Command command, String label, String[] args) {
+    private void cmdReload(CommandSender sender) {
         if (!PermissionNode.RELOAD.isAllowedAlert(sender)) return;
 
         plugin.reloadConfig();
@@ -158,7 +182,7 @@ public class CmdNickTokens implements CommandExecutor {
     /*
      * Remove tokens from a player's balance.
      */
-    private void cmdRemove(CommandSender sender, Command command, String label, String[] args) {
+    private void cmdRemove(CommandSender sender, String label, String[] args) {
         if (!PermissionNode.TOKENS_REMOVE.isAllowedAlert(sender)) return;
 
         if (args.length < 2) {
@@ -211,7 +235,7 @@ public class CmdNickTokens implements CommandExecutor {
     /*
      * Reset a player's balance.
      */
-    private void cmdReset(CommandSender sender, Command command, String label, String[] args) {
+    private void cmdReset(CommandSender sender, String label, String[] args) {
         if (!PermissionNode.TOKENS_RESET.isAllowedAlert(sender)) return;
 
         String targetName;
@@ -238,12 +262,26 @@ public class CmdNickTokens implements CommandExecutor {
     /*
      * Save plugin data.
      */
-    private void cmdSave(CommandSender sender, Command command, String label, String[] args) {
+    private void cmdSave(CommandSender sender) {
         if (!PermissionNode.SAVE.isAllowedAlert(sender)) return;
 
         plugin.getTokenManager().save();
         plugin.saveConfig();
         Messages.PLUGIN_SAVED.sendTo(sender);
+    }
+
+    private void cmdVersion(CommandSender sender) {
+        sender.sendMessage("" + ChatColor.GREEN + ChatColor.BOLD + "NickTokens" + ChatColor.GOLD + ChatColor.ITALIC + " v" + plugin.getDescription().getVersion());
+        sender.sendMessage(ChatColor.BLUE + "Created by Stealth2800");
+        sender.sendMessage("" + ChatColor.AQUA + ChatColor.UNDERLINE + "http://stealthyone.com/");
+
+        UpdateChecker updateChecker = plugin.getUpdateChecker();
+        if (updateChecker.checkForUpdates()) {
+            String curVer = plugin.getDescription().getVersion();
+            String remVer = updateChecker.getNewVersion().replace("v", "");
+            sender.sendMessage(ChatColor.RED + "A different version was found on BukkitDev! (Current: " + curVer + " | Remote: " + remVer + ")");
+            sender.sendMessage(ChatColor.RED + "You can download it from " + updateChecker.getVersionLink());
+        }
     }
 
 }
